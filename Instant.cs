143c8d0975c23ae872747fbe2ba2247fe12c94eb
GoodOlClint <goodolclint@gmail.com>
2014-02-02 00:10:10 -0600

    Sidereal Time
    Added class to represent an instance of a day. Used to calculate
    Sidereal time

---
+++ Instant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astronomy
+{
+    public class Instant
+    {
+
+        public double TotalSeconds { get; protected set; }
+        public int Hour { get { return (int)(Math.Floor(this.TotalSeconds) / 3600) % 24; } }
+        public int Minute { get { return (int)(Math.Floor(this.TotalSeconds / 60)) % 60; } }
+
+        public double Second { get { return this.TotalSeconds % 60; } }
+
+        public Instant(int Hour, int Minute, double Second)
+        {
+            this.AddSeconds(Second + (Minute * 60) + (Hour * 3600));
+        }
+
+        public Instant(double Seconds)
+        { this.AddSeconds(Seconds); }
+
+        public void AddSeconds(double Second)
+        {
+            this.TotalSeconds = (this.TotalSeconds + Second) % 86400;
+            if (this.TotalSeconds < 0)
+            { this.TotalSeconds += 86400; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,10 +3,28 @@
 ##Julian Day
 Allows calculations of any date after noon on January 1, 4713 BC.
 
-```charp
+```csharp
     JulianDay JD = new JulianDay(333, 1, 27, 12);
     Debug.Assert(JD.JulianDayNumber == 1842713.0);
 
     JulianDay JD1 = new JulianDay(1957, 10, 4).AddDays(0.81);
     Debug.Assert(JD1.JulianDayNumber == 2436116.31);
+```
+
+##Instant
+Helper class for calculating Sidereal time
+
+```csharp
+	JulianDay JD = new JulianDay(2446895.5);
+    double T = (JD.JulianDayNumber - 2451545.0) / 36525;
+
+
+    Instant sidereal = new Instant(6, 41, 50.54841);
+    sidereal.AddSeconds(8640184.812866 * T);
+    sidereal.AddSeconds(0.093104 * Math.Pow(T,2));
+    sidereal.AddSeconds(0.0000062 * Math.Pow(T, 3));
+
+    Instant sidereal1 = new Instant(1.00273790935 * new Instant(19, 21, 0).TotalSeconds);
+
+    Instant sidereal2 = new Instant(sidereal1.TotalSeconds + sidereal.TotalSeconds);
 ```
